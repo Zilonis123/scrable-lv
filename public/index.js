1b7e036f4301f301e0c9299c84f5e37fe90c719a
Zilonis123 <kazaks.karlis2009@gmail.com>
2023-12-31 20:56:23 +0200

    Simple implementation

--- public/index.js
+++ public/index.js
@@ -2,13 +2,95 @@
 async function search() {
     // Function that starts it all
     
-    const letters = document.getElementById("letters").value;
+    // Get the letters
+    var letters_input = document.getElementById("letters");
+    var letters = letters_input.value;
+
+    // Check if letters isn't larger than 7 letters because theres no need to check for 8! when 7! is smaller and scrable ...
+    // .. only allows 7 letters per person
+    if (letters.length > 7) return;
+
+    // Reset the input and disable it
+    letters_input.value = "";
+    letters_input.disabled = true;
+
+    var words = await get_words(letters);
+
+    // Show the result to the user
+    document.getElementById("words").innerText = words.join(", ")
+
+    // enable the input
+    letters_input.disabled = false;
+
 }
 
 
+async function get_words(letters) {
+    
+    // Generate all arangements without duplicates
+    var arrangements = getAllUniquePermutations(letters);
+    
+
+    // Check all the arrangements and see if there are any real words
+    const validWords = [];
+    
+    for (let i=0; i < arrangements.length; i++) {
+        var arr = arrangements[i]
+        var isValid = await checkWordValidity(arr);
+        if (isValid) validWords.push(arr);
+    }
+
+    return validWords
+}
+
+function getAllUniquePermutations(str) {
+    const result = new Set();
+
+    function swapChars(arr, i, j) {
+        const temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    function generatePermutations(charArray, startIndex) {
+        if (startIndex === charArray.length - 1) {
+        result.add(charArray.join(''));
+        return;
+        }
+
+        for (let i = startIndex; i < charArray.length; i++) {
+        swapChars(charArray, startIndex, i);
+        generatePermutations([...charArray], startIndex + 1);
+        swapChars(charArray, startIndex, i); // Backtrack
+        }
+    }
+
+    generatePermutations([...str], 0);
+    return Array.from(result);
+}
+    
+async function checkWordValidity(word) {
+    try {
+        var data = await find(word);
+        var isValid = data.length >= 1 && word.length > 3;
+        return isValid
+    } catch (err) {
+        return false
+    }
+    
+}
+
 async function find(word) {
-    const res = await fetch(`http://api.tezaurs.lv:8182/v1/inflections/${word}`);
+    const res = await fetch(`http://api.tezaurs.lv:8182/analyze/${word}`);
     const json = await res.json();
     return json;
 }
 
+
+function factorial(n) {
+    let result = 1;
+    for (let i = 2; i <= n; i++) {
+        result *= i;
+    }
+    return result;
+}
